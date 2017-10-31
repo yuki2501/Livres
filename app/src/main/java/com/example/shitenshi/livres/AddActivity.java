831353383f30f009317db800badcc62305fe2bf3
shitenshi <yuuki.1226.saito@gmail.com>
2017-10-31 17:40:28 +0900

    Bug fix. Add AddActivity.

--- app/src/main/java/com/example/shitenshi/livres/AddActivity.java
+++ app/src/main/java/com/example/shitenshi/livres/AddActivity.java
@@ -7,27 +7,34 @@ import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import static com.example.shitenshi.livres.R.id.editText;
+
 public class AddActivity extends AppCompatActivity {
     private static final int MAIN_ACTIVITY = 101;
-
+    Outgodbhelper outgodbhelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcard);
+        outgodbhelper = new Outgodbhelper(this);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText editText = (EditText)findViewById(R.id.editText);
-                Editable text1 = editText.getText();
-
-                Intent intent1 = new Intent(getApplication(), MainActivity.class);
-                //get
-                intent1.putExtra("number", text1);
-                startActivityForResult(intent1,MAIN_ACTIVITY);
+                Spinner spinner =(Spinner)findViewById(R.id.spinner);
+                int number = spinner.getSelectedItemPosition();
+                EditText n = (EditText) findViewById(R.id.editText2);
+                Editable productname = n.getText();
+                EditText p = (EditText) findViewById(R.id.editText);
+                Editable price = p.getText();
+                outgodbhelper.insertValues(Integer.valueOf(number),productname.toString(),Integer.valueOf(price.toString()));
+                Toast.makeText(AddActivity.this, "ok", Toast.LENGTH_LONG).show();
+                finish();
 
              }
 
--- app/src/main/java/com/example/shitenshi/livres/Outgodbhelper.java
+++ app/src/main/java/com/example/shitenshi/livres/Outgodbhelper.java
@@ -1,25 +1,35 @@
 package com.example.shitenshi.livres;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.text.Editable;
 
 /**
  * Created by shitenshi on 17/10/24.
  */
 
 public class Outgodbhelper extends SQLiteOpenHelper {
-    private static final String CREATE_TABLE_SQL = "" +  "create table outgodb(" +
+    public static final String TABLE_NAME = "shyushi_table";
+    public static final String CATEGORY_KEY = "category";
+    public static final String PRODUCTNAME_KEY = "productname";
+    public static final String PRICE_KEY = "price";
+    private static final String CREATE_TABLE_SQL = "" +  "create table " + TABLE_NAME + "(" +
             "rowid integer primary key autoincrement," +
-            "data text null" +
+            CATEGORY_KEY + " string," +
+            PRODUCTNAME_KEY + " string," +
+            PRICE_KEY + " integer" +
+            
+
             ")";
-    private static final String DROP_TABLE_SQL = "drop table if exists outgodb";
+    private static final String DROP_TABLE_SQL = "drop table if exists" + TABLE_NAME;
 
 
 
 
-    public Outgodbhelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public Outgodbhelper(Context context) {
+        super(context, "syushidb", null, 1);
     }
 
     @Override
@@ -30,6 +40,15 @@ public class Outgodbhelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL(DROP_TABLE_SQL);
+
+    }
+    public void insertValues(Integer category, String Productname , Integer Price){
+        ContentValues values = new ContentValues();
+        values.put(Outgodbhelper.CATEGORY_KEY,category);
+        values.put(Outgodbhelper.PRODUCTNAME_KEY,Productname);
+        values.put(Outgodbhelper.PRICE_KEY,Price);
+        getWritableDatabase().insert(Outgodbhelper.TABLE_NAME,null,values);
+
 
     }
 }
