bba3026b65171d8f7fba08bf5e51ef8be4fa23ed
masuken111 <msc.emk@icloud.com>
2017-11-18 13:29:04 +0900

    Bug Fix.

--- app/src/main/java/com/example/shitenshi/livres/Outgodbhelper.java
+++ app/src/main/java/com/example/shitenshi/livres/Outgodbhelper.java
@@ -18,11 +18,13 @@ public class Outgodbhelper extends SQLiteOpenHelper {
     public static final String CATEGORY_KEY = "category";
     public static final String PRODUCTNAME_KEY = "productname";
     public static final String PRICE_KEY = "price";
+    public static final String REMAININGMONEY_KEY = "remainingmoney";
     private static final String CREATE_TABLE_SQL = "" +  "create table " + TABLE_NAME + "(" +
             "rowid integer primary key autoincrement," +
             CATEGORY_KEY + " string," +
             PRODUCTNAME_KEY + " string," +
             PRICE_KEY + " integer" +
+            REMAININGMONEY_KEY + "integer" +
             
 
             ")";
@@ -51,6 +53,7 @@ public class Outgodbhelper extends SQLiteOpenHelper {
         values.put(Outgodbhelper.CATEGORY_KEY,dbContainer.category);
         values.put(Outgodbhelper.PRODUCTNAME_KEY,dbContainer.productname);
         values.put(Outgodbhelper.PRICE_KEY,dbContainer.price);
+        values.put(Outgodbhelper.REMAININGMONEY_KEY,dbContainer.remainingmoney);
         getWritableDatabase().insert(Outgodbhelper.TABLE_NAME,null,values);
 
 
@@ -67,7 +70,8 @@ public class Outgodbhelper extends SQLiteOpenHelper {
             list.add(new DbContainer(
                     cursor.getString(1),
                     cursor.getString(2),
-                    cursor.getInt(3)
+                    cursor.getInt(3),
+                    cursor.getInt(4)
             ));
         }
         return list;
