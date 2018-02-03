a2d7d014cbc62d3fac53e5dab82179486e021047
moko256 <koutaro.mo@gmail.com>
2018-02-04 02:13:09 +0900

    refactor: class nameで判定する方式から、instanceofで判断する方式にした

--- app/src/main/java/com/example/shitenshi/livres/Theme.java
+++ app/src/main/java/com/example/shitenshi/livres/Theme.java
@@ -8,37 +8,34 @@ import android.preference.PreferenceManager;
  */
 
 class Theme {
-    private static String getClassName(){
-        return Thread.currentThread().getStackTrace()[2].getClassName();
-    }
     void themeset(Context context){
          String listpre = PreferenceManager
                 .getDefaultSharedPreferences(context)
                 .getString("list_preference_1","akari");
         switch (listpre){
             case "oumurasaki":
-                if (getClassName().equals("AddActivity")) {
+                if (context instanceof AddActivity) {
                     context.setTheme(R.style.AppTheme);
                 }
-                if (getClassName().equals("PreferenceActivity")) {
+                if (context instanceof PreferenceActivity) {
                     context.setTheme(R.style.AppTheme_PreferenceTheme);
                 }else{
                 context.setTheme(R.style.AppTheme_NoActionBar);
                 }
             case "akari":
-                if (getClassName().equals("AddActivity")) {
+                if (context instanceof AddActivity) {
                     context.setTheme(R.style.AppTheme_Akari);
                 }
-                if (getClassName().equals("PreferenceActivity")) {
+                if (context instanceof PreferenceActivity) {
                     context.setTheme(R.style.AppTheme_PreferenceTheme_Akari);
                 }else{
                     context.setTheme(R.style.AppTheme_Akari_NoActionBar);
                 }
             case "kaeru":
-                if (getClassName().equals("AddActivity")) {
+                if (context instanceof AddActivity) {
                     context.setTheme(R.style.AppTheme_kaeru);
                 }
-                if (getClassName().equals("PreferenceActivity")) {
+                if (context instanceof PreferenceActivity) {
                     context.setTheme(R.style.AppTheme_PreferenceTheme_kaeru);
                 }else{
                     context.setTheme(R.style.AppTheme_kaeru_NoActionBar);
